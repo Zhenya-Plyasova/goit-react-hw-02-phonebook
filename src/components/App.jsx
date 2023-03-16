8117f568c6b137928d0dee61b14dc434f62bbc2f
Zhenya Plyasova <111362107+Zhenya-Plyasova@users.noreply.github.com>
2023-03-16 18:07:11 +0200

    resolved

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,62 @@
-export const App = () => {
+import { Component } from "react";
+import { nanoid } from 'nanoid';
+import { ContactForm } from "./ContactForm/ContactForm";
+import { ContactList } from "./ContactList/ContactList";
+import { Filter } from "./Filter/Filter";
+
+export class App extends Component {
   state = {
     contacts: [],
-    name: ''
+    filter: ''
+  };
+
+  createContact = data => {
+    const checkNewContact = this.state.contacts.find(({name})=>name===data.name);
+    if (checkNewContact){
+      return alert (`${data.name} is already in contacts`)
+    };
+    const newContact = {
+      id: nanoid(),
+      ...data,
+    };
+    console.log(newContact);
+
+    this.setState(({contacts})=>({
+      contacts: [...contacts, newContact]
+    }));
+  };
+
+  handleDelete = (id) => {
+    this.setState((prev)=>({
+      contacts: prev.contacts.filter((contact)=>contact.id!==id),
+    }))
+  };
+
+  filterContacts = (e) =>{
+    this.setState({filter: e.currentTarget.value.toLowerCase()});
   }
-  
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+
+  render() {
+    const {contacts, filter}=this.state;
+    const filteredContacts = contacts.filter(contact=>contact.name.toLowerCase().includes(this.state.filter));
+
+    return (
+      <div
+        style={{
+          // height: '100vh',
+          fontSize: 20,
+          color: '#010101',
+          margin: 30,
+        }}
+      >
+        <h2>Phonebook</h2>
+        <ContactForm createContact={this.createContact} />
+
+        <h2>Contacts</h2>
+
+        <Filter onChange={this.filterContacts} value = {filter} />
+        <ContactList handleDelete= {this.handleDelete} contactsList = {filteredContacts} />
+      </div>
+    );
+  }
 };
